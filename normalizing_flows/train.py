0884fd628a728c966e7fe7cd323282385722850a
Young Seok Tony Kim <93mighty@gmail.com>
2021-02-09 16:23:54 -0800

    Modified training script to follow pytorch-flows

--- normalizing_flows/train.py
+++ normalizing_flows/train.py
@@ -6,13 +6,8 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 
-from maf import MAF
+import flows as fnn
 from datasets import get_datasets, get_input_size
-
-def debug(tensor):
-    print('printing tensor')
-    print(type(tensor))
-    print(tensor.shape)
 
 def main(args):
     # Get Dataset
@@ -26,36 +21,35 @@ def main(args):
     test_loader = DataLoader(test, batch_size=args.batch_size)
 
     # Define model
-    model = MAF(input_size, args.made_blocks, args.hidden_dims)
+    modules = []
+    for _ in range(args.made_blocks):
+        modules += [
+            fnn.MADE(input_size, args.hidden_dims, num_cond_inputs=None, act='relu'),
+            fnn.BatchNormFlow(input_size),
+            fnn.Reverse(input_size)
+        ]
+    model = fnn.FlowSequential(*modules)
 
     # Define optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
     
-
     # Train model
+    model.train()
     for epoch_num in tqdm(range(1, args.epoch+1)):
-        model.train()
         train_loss = 0
         for batch, _ in train_loader:
-            #print("batch.shape",batch.shape # [128, 784]
-            # Forward pass
-            u, log_det = model.forward(batch)
-            #print('u, log_det', u.shape, log_det.shape) # torch.Size([128, 784]) / torch.Size([128])
+            optimizer.zero_grad()
 
             # Loss = Negative Log Likelihood
-            loss = 0.5 * (u ** 2).sum(dim=1)
-            loss += 0.5 * batch.shape[1] * np.log(2 * math.pi)
-            loss -= log_det
-            loss = torch.mean(loss)
+            loss = -model.log_probs(batch).mean()
             train_loss += loss.item()
 
             # Backpropagation
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
 
         avg_loss = np.sum(train_loss) / len(train_loader)
-        print(f"Epoch: {epoch_num} Average loss: {avg_loss:.5f}")
+        print(f"Epoch: {epoch_num} Average log likelihood: {-avg_loss:.5f}")
 
 
 if __name__ == "__main__":
@@ -67,6 +61,6 @@ if __name__ == "__main__":
     parser.add_argument('--learning_rate', default=1e-4, type=float, help="Learning rate for the optimizer")
     parser.add_argument('--weight_decay', default=1e-6, type=float, help="Weight decay for the optimizer")
     parser.add_argument('--made_blocks', default=5, type=int, help='Number of MADE blocks for the MAF model')
-    parser.add_argument('--hidden_dims', default=[512], nargs="+", help='Number of nodes for hidden layers for each MADE block')
+    parser.add_argument('--hidden_dims', default=512, type=int, help='Number of nodes for hidden layers for each MADE block')
     args = parser.parse_args()
     main(args)
