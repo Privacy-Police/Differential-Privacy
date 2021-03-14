147fd47da1ebc82833c68f3c91f0c906ccfc9805
Chavi Gupta <chavig080@gmail.com>
2021-03-14 13:44:46 -0700

    Modifying code for diagnostic plots

--- normalizing_flows/utils.py
+++ normalizing_flows/utils.py
@@ -9,40 +9,53 @@ warnings.filterwarnings("ignore")
 gpu_available = torch.cuda.is_available()
 device = "cuda" if gpu_available else "cpu"
 
-def get_distribution_diagnostic_plot(model_path, input, data_name = 'MNIST', model_name = 'maf', ncols = 6, nrows = 4):
+# Function to plot the distributions for the base density 'u' of the input dataset
+def get_distribution_diagnostic_plot(model_path, input, data_name = 'MNIST', model_name = 'maf', ncols = 6, nrows = 1):
   model = torch.load(model_path, map_location=torch.device(device))
 
   model.to(device)
   model.eval()
+  ncols = input.shape[1]
+  
+  #Obtain u by backpropagation
   u = model(input.to(device))[0].detach().cpu().numpy()
-
+  
   fig, axes = plt.subplots(
-        ncols=ncols, nrows=nrows, sharex=True, sharey=True, figsize=(16, 10)
+        ncols=ncols, nrows=nrows, sharex=True, sharey=True, figsize=(16, 8)
     )
   
+  i=0
   for ax in axes.reshape(-1):
-    dim1 = np.random.randint(input.shape[1])  
+    dim1 = i
+    i=i+1
     sns.distplot(u[:, dim1], ax=ax, color="darkorange")
     ax.set_xlabel("dim: " + str(dim1), size=14)
     ax.set_xlim(-5, 5)
   fig.suptitle("Distribution plot for " + data_name + " Dataset", fontsize = 13)
-  plt.savefig("figs/" + model_name + "_" + data_name + "_marginal.png", dpi=300)
-  plt.savefig("figs/" + model_name + "_" + data_name + "_marginal.pdf", dpi=300)
+  outpath = "figs/" + model_name + "_" + data_name + "_marginal"
+  plt.savefig(outpath + ".png", dpi=300)
+  #plt.savefig(outpath + ".pdf", dpi=300)
+  print("Distribution plots saved to ", outpath)
 
-
-def get_scattered_diagnostic_plot(model_path, input, data_name = 'MNIST', model_name = 'maf', ncols = 6, nrows = 4):
+# Function to plot the pairwise scatterplot for the base density 'u' of the input dataset
+def get_scattered_diagnostic_plot(model_path, input, data_name = 'MNIST', model_name = 'maf', ncols = 6, nrows = 6):
   model = torch.load(model_path, map_location=torch.device(device))
   model.to(device)
   model.eval()
-  u = model(input.to(device))[0].detach().cpu().numpy()
   
+  #Obtain u by backpropagation
+  u = model(input.to(device))[0].detach().cpu().numpy()
+  ncols = input.shape[1]
+  nrows = input.shape[1]
   fig, axes = plt.subplots(
         ncols=ncols, nrows=nrows, sharex=True, sharey=True, figsize=(16, 10)
     )
 
+  i=0
   for ax in axes.reshape(-1):
-        dim1 = np.random.randint(input.shape[1])
-        dim2 = np.random.randint(input.shape[1])
+        dim1 = int(i%ncols)
+        dim2 = int(i/ncols)  
+        i=i+1
         ax.scatter(u[:, dim1], u[:, dim2], color="dodgerblue", s=0.5)
         ax.set_ylabel("dim: " + str(dim2), size=14)
         ax.set_xlabel("dim: " + str(dim1), size=14)
@@ -50,5 +63,7 @@ def get_scattered_diagnostic_plot(model_path, input, data_name = 'MNIST', model_
         ax.set_ylim(-8, 8)
         ax.set_aspect(1)
   fig.suptitle("Scatter plot for " + data_name + " Dataset", fontsize = 13)
-  plt.savefig("figs/" + model_name + "_" + data_name + "_scatter.png", dpi=300)
-  plt.savefig("figs/" + model_name + "_" + data_name + "_scatter.pdf", dpi=300)
+  outpath = "figs/" + model_name + "_" + data_name + "_scatter"
+  plt.savefig(outpath + ".png", dpi=300)
+  #plt.savefig(outpath + ".pdf", dpi=300)
+  print("Scatter plot saved to ", outpath)
